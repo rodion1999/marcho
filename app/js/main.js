9ac8ccd25f066c6265d01b47badaafac7f57bef4
rodion1999 <rodionpustovoj7@gmail.com>
2022-04-27 19:38:57 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,10 @@ $(function () {
     autoplaySpeed: 2000
 
   });
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
 });
