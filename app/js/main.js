693f382d9c1dd8f03e82c8130e4cd84dd8a8340d
rodion1999 <rodionpustovoj7@gmail.com>
2022-05-27 21:02:38 +0300

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
 $(function () {
+  $('.shop__filter-btn').on('click', function () {
+    $('.shop__filters').slideToggle();
+  });
   
   $('.menu__btn').on('click', function(){
     $('.menu__list').toggleClass('menu__list--active');
@@ -32,7 +35,15 @@ $(function () {
     asNavFor: '.product-slide__thumb',
     draggable: false,
     arrows: false,
-    fade: true
+    fade: true,
+      responsive: [
+    {
+      breakpoint: 1051,
+      settings: {
+        draggable: true,
+      }
+      },
+    ]
   });
 
   $('.shop-content__filter-btn').on('click', function () {
@@ -41,9 +52,11 @@ $(function () {
   });
   $('.button-list').on('click', function () {
     $('.product-item').addClass('product-item--list');
+    $('.shop-content__inner').addClass('shop-content__nogrid');
   });
   $('.button-grid').on('click', function () {
     $('.product-item').removeClass('product-item--list');
+    $('.shop-content__inner').removeClass('shop-content__nogrid');
   });
 
 
